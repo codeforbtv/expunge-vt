3f2b64ddc6e4959efe39dfc238c3f0fab81d2d51
Micah Mutrux <micah.mutrux@gmail.com>
2020-12-17 16:37:21 -0500

    minor formatting

--- extensionDirectory/components.js
+++ extensionDirectory/components.js
@@ -107,11 +107,11 @@ Vue.component('filing-footer', {
 Vue.component('filing-dated-city', {
   template: `
   <p class="filing-dated-city indent">
-  Dated in the city of <span class="fill-in quarter-width"></span>, 
-  on the day of <span class="fill-in digit-width"></span>, 
-  in the month of <span class="fill-in word-width"></span>,<br/>
-  in the year 20<span class="fill-in digit-width"></span>.
-</p>
+    Dated in the city of <span class="fill-in quarter-width"></span>,
+    on the day of <span class="fill-in digit-width"></span>,
+    in the month of <span class="fill-in word-width"></span>,<br/>
+    in the year 20<span class="fill-in digit-width"></span>.
+  </p>
   `,
 });
 
