049d0f8529c30f4794a43744a586b9954be09e72
JOD <39913522+jakedurell@users.noreply.github.com>
2020-12-17 15:48:57 -0500

    date toggle stuff

--- extensionDirectory/components.js
+++ extensionDirectory/components.js
@@ -106,7 +106,12 @@ Vue.component('filing-footer', {
 
 Vue.component('filing-dated-city', {
   template: `
-    <p class="filing-dated-city indent">Dated in <span class="fill-in">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>, this <span class="fill-in">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> day of <span class="fill-in">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>, 20<span class="fill-in">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>.</p>
+  <p class="filing-dated-city indent">
+  Dated in the city of <span class="fill-in quarter-width"></span>, 
+  on the day of <span class="fill-in digit-width"></span>, 
+  in the month of <span class="fill-in word-width"></span>,<br/>
+  in the year 20<span class="fill-in digit-width"></span>.
+</p>
   `,
 });
 
